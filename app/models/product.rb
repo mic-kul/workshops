f854dca87d3c5fb01f298239694a11b889bbc42f
Michal Kulesza <mk@trou.pl>
2014-11-28 16:44:38 +0100

    product validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,16 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+
+  validates_presence_of :price, :title, :description
+  validates_numericality_of :price
+
+  def average_rating
+    sum = .0
+    reviews.each do |review|
+      sum += review.rating
+    end
+    return sum / reviews.count
+  end
+
 end
